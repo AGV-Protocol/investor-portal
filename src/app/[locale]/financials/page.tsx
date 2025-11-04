a243b750af4c8c9f9b6d4dc050aa3b17ac8ae474
phantom-fort <phantomayoola@gmail.com>
2025-11-04 21:58:23 +0100

    Add new MOU document for green power direct connection to sales & marketing section, enhancing resource accessibility.

--- src/app/[locale]/financials/page.tsx
+++ src/app/[locale]/financials/page.tsx
@@ -33,7 +33,52 @@ export default function FinancialsPage() {
   };
 
   // Document sections
-  const documentSections: DocumentSection[] = [
+  const visibleLimit = 5;
+
+  const splitDocs = (docs: Document[]) => {
+    const visible = docs.slice(0, visibleLimit);
+    const more = docs.length > visibleLimit ? docs.slice(visibleLimit) : undefined;
+    return { documents: visible, moreDocuments: more };
+  };
+
+  const onChainDocs: Document[] = [
+    { title: "日度发电量快照表_10月31日", description: "Daily power generation snapshot - October 31st", url: "https://drive.google.com/file/d/1dAn1IRKBxrD2vAizoGgWZHVisK3OOZEQ/view" },
+    { title: "日度发电量快照表_10月31日 (Word Doc)", description: "Daily power generation snapshot - October 31st (Word Document)", url: "https://docs.google.com/document/d/1RzRNIHQYVjaMcNwVJ2R-fbTvIZVacrlW/edit?usp=drive_link&ouid=103368231820708863962&rtpof=true&sd=true" },
+    { title: "日度发电量快照表_10月30日", description: "Daily power generation snapshot - October 30th", url: "https://drive.google.com/file/d/1QDx8Z2h2asK-5NiJnw2GWFfdaWm-xRQO/view" },
+    { title: "日度发电量快照表_10月29日", description: "Daily power generation snapshot - October 29th", url: "https://drive.google.com/file/d/1ZHT3LSCQH012-m6TXzKsP9pFXCcNrxZo/view" },
+    { title: "日度发电量快照表_10月28日", description: "Daily power generation snapshot - October 28th", url: "https://drive.google.com/file/d/1U6-6ZT8EthKW33E4CPA1LPEQOfQl0Uy2/view" },
+    { title: "日度发电量快照表_10月27日", description: "Daily power generation snapshot - October 27th", url: "https://drive.google.com/file/d/1OUdSug7ZoOL9OrFU8VMM1KUXM8GZmCHF/view?usp=drive_link" },
+    { title: "日度发电量快照表_10月26日", description: "Daily power generation snapshot - October 26th", url: "https://drive.google.com/file/d/1MLGMma--VBwYiBZrdTDCKSdcOvJTgKPG/view" },
+    { title: "日度发电量快照表_10月25日", description: "Daily power generation snapshot - October 25th", url: "https://drive.google.com/file/d/1xhHoiXgljZxWc0-3yhdkLtcIPwMJ8nwP/view" },
+    { title: "日度发电量快照表_10月24日", description: "Daily power generation snapshot - October 24th", url: "https://drive.google.com/file/d/1hihkL6bZShU6FTTYRVhYizK5lmLMy9yO/view" },
+    { title: "日度发电量快照表_10月23日", description: "Daily power generation snapshot - October 23rd", url: "https://drive.google.com/file/d/156Eq2JfslzqFGXZARTXW67K6PFP8-7vJ/view" },
+    { title: "日度发电量快照表_10月22日", description: "Daily power generation snapshot - October 22nd", url: "https://drive.google.com/file/d/11Xud71w9T6z0rnn6s4Ai8Dbo31a9i1Oo/view?usp=drive_link" },
+    { title: "日度发电量快照表_10月21日", description: "Daily power generation snapshot - October 21st", url: "https://drive.google.com/file/d/1-HfyyP1W71rk-lvT0lKh0n7merZAx7u9/view" },
+    { title: "日度发电量快照表_10月20日", description: "Daily power generation snapshot - October 20th", url: "https://drive.google.com/file/d/1PMqwrCQm7ve3njjgSnw3X0ZcTR6jdXVb/view?usp" },
+    { title: "日度发电量快照表_10月19日", description: "Daily power generation snapshot - October 19th", url: "https://drive.google.com/file/d/125AHWlvlbE8OFrO-rc2Pc5Lo_p-4E9RL/view" },
+    { title: "日度发电量快照表_10月18日", description: "Daily power generation snapshot - October 18th", url: "https://drive.google.com/file/d/1p768iALX8NW7qQlpO_UpWUB1Apm6H8n8/view" },
+    { title: "日度发电量快照表_10月17日", description: "Daily power generation snapshot - October 17th", url: "https://drive.google.com/file/d/12DVuw0z8bPgrbmfl0yOYqXVM32Jaw-C6/view" },
+    { title: "日度发电量快照表_10月16日", description: "Daily power generation snapshot - October 16th", url: "https://drive.google.com/file/d/1TCmrvCB9OpHZzwA3Gn-uVaaa7HSBAuh0/view" },
+    { title: "日度发电量快照表_10月15日", description: "Daily power generation snapshot - October 15th", url: "https://drive.google.com/file/d/1NaFhzdMfNwRLCBxzsd3BECd-Jvv1t9FV/view" },
+    { title: "日度发电量快照表_10月14日", description: "Daily power generation snapshot - October 14th", url: "https://drive.google.com/file/d/1yTcYbA6Ljr-aLf8XtXDcrcbjkd3Q7apV/view" },
+    { title: "日度发电量快照表_10月13日", description: "Daily power generation snapshot - October 13th", url: "https://drive.google.com/file/d/14gZ_43lb-C7FpO0KsH-7rqRROntDPcdf/view" },
+    { title: "日度发电量快照表_10月12日", description: "Daily power generation snapshot - October 12th", url: "https://drive.google.com/file/d/1ospfRukexHunQNVKBVJveN5T8Qp_Ehlx/view" },
+    { title: "日度发电量快照表_10月11日", description: "Daily power generation snapshot - October 11th", url: "https://drive.google.com/file/d/1bK238GoE_vc9BktKnU7UF9MGZblLoB0O/view" },
+    { title: "日度发电量快照表_10月10日", description: "Daily power generation snapshot - October 10th", url: "https://drive.google.com/file/d/1WffubVm_LT8PObAYI7ddwbVkTrIr4Ixw/view" },
+    { title: "日度发电量快照表_10月09日", description: "Daily power generation snapshot - October 9th", url: "https://drive.google.com/file/d/1oMYEoBaEF_UWNOpV0LXqDZU9KcBaeHf4/view" },
+    { title: "日度发电量快照表_10月08日", description: "Daily power generation snapshot - October 8th", url: "https://drive.google.com/file/d/1ssYYjXZvYugxF-BpZNRycJGjwxKRU3CR/view" },
+    { title: "日度发电量快照表_10月07日", description: "Daily power generation snapshot - October 7th", url: "https://drive.google.com/file/d/1pKI5NsURP9dC2PmtswCtZi-gdlak64ky/view" },
+    { title: "日度发电量快照表_10月06日", description: "Daily power generation snapshot - October 6th", url: "https://drive.google.com/file/d/1OyuA6uvA9GwYo3M6ufqPFELgQibE_sn7/view" },
+    { title: "日度发电量快照表_10月05日", description: "Daily power generation snapshot - October 5th", url: "https://drive.google.com/file/d/1Y-PrXE2Ci-KJRbSGg8PzwoG19GV7kW_v/view" },
+    { title: "日度发电量快照表_10月04日", description: "Daily power generation snapshot - October 4th", url: "https://drive.google.com/file/d/1VjqqErRQmV1LVORx92iiXb_JLVfd0UmT/view" },
+    { title: "日度发电量快照表_10月03日", description: "Daily power generation snapshot - October 3rd", url: "https://drive.google.com/file/d/1GUlf-qhUjF4F9QtJguUh8KV7h2VFTrhH/view" },
+    { title: "日度发电量快照表_10月02日", description: "Daily power generation snapshot - October 2nd", url: "https://drive.google.com/file/d/14hrBdDW2J-YVkhokPRWLk--FqvArKhQa/view" }
+  ];
+
+  const { documents: onChainVisible, moreDocuments: onChainMore } = splitDocs(onChainDocs);
+
+  // use `any` here so we can put JSX (details) into the description for the expandable UI
+  const documentSections: any[] = [
     {
       title: "Consolidated Audit Report",
       description: "China Commerce Holdings / Zhongshang Puhui Group audit documentation",
@@ -42,6 +87,26 @@ export default function FinancialsPage() {
           title: "中商普惠公司增资扩股尽调报告(5)",
           description: "China Commerce Holdings due diligence report for capital increase and share expansion",
           url: "https://drive.google.com/file/d/1bjBb_A6sJdGEw6PNBCxL68gTVZewO5YH/view"
+        },
+        {
+          title: "Balance Sheet Original Copy.pdf",
+          description: "AGV Protocol balance sheet as of October 2025",
+          url: "https://drive.google.com/file/d/10ioydWY8LNwX8pX22nvFZeagNvR6P81j/view"
+        },
+        {
+          title: "Balance Sheet OCT2025.pdf",
+          description: "AGV Protocol balance sheet as of October 2025",
+          url: "https://drive.google.com/file/d/1IJmdiazPVJuHNONdtlOEON20Zy9nQqmA/view"
+        },
+        {
+          title: "Income Statement Original Copy.pdf",
+          description: "AGV Protocol income statement for the period ending October 2025",
+          url: "https://drive.google.com/file/d/1WCPiUKrYpV5nkgERwuO546q9_WrloZJO/view"
+        },
+        {
+          title: "Income Statement OCT2025.pdf",
+          description: "AGV Protocol income statement for the period ending October 2025",
+          url: "https://drive.google.com/file/d/1k_RdnenfxfBXpvsZ1xEBo1pXPNLZIj-d/view"
         }
       ]
     },
@@ -58,174 +123,34 @@ export default function FinancialsPage() {
     },
     {
       title: "On-chain Power Generation Verification (Power-to-Mint Pilot)",
-      description: "Daily power generation snapshots and verification documentation",
-      documents: [
-        {
-          title: "日度发电量快照表_10月31日",
-          description: "Daily power generation snapshot - October 31st",
-          url: "https://drive.google.com/file/d/1dAn1IRKBxrD2vAizoGgWZHVisK3OOZEQ/view"
-        },
-        {
-          title: "日度发电量快照表_10月31日 (Word Doc)",
-          description: "Daily power generation snapshot - October 31st (Word Document)",
-          url: "https://docs.google.com/document/d/1RzRNIHQYVjaMcNwVJ2R-fbTvIZVacrlW/edit?usp=drive_link&ouid=103368231820708863962&rtpof=true&sd=true"
-        },
-        {
-          title: "日度发电量快照表_10月30日",
-          description: "Daily power generation snapshot - October 30th",
-          url: "https://drive.google.com/file/d/1QDx8Z2h2asK-5NiJnw2GWFfdaWm-xRQO/view"
-        },
-        {
-          title: "日度发电量快照表_10月29日",
-          description: "Daily power generation snapshot - October 29th",
-          url: "https://drive.google.com/file/d/1ZHT3LSCQH012-m6TXzKsP9pFXCcNrxZo/view"
-        },
-        {
-          title: "日度发电量快照表_10月28日",
-          description: "Daily power generation snapshot - October 28th",
-          url: "https://drive.google.com/file/d/1U6-6ZT8EthKW33E4CPA1LPEQOfQl0Uy2/view"
-        },
-        {
-          title: "日度发电量快照表_10月27日",
-          description: "Daily power generation snapshot - October 27th",
-          url: "https://drive.google.com/file/d/1OUdSug7ZoOL9OrFU8VMM1KUXM8GZmCHF/view?usp=drive_link"
-        },
-        {
-          title: "日度发电量快照表_10月26日",
-          description: "Daily power generation snapshot - October 26th",
-          url: "https://drive.google.com/file/d/1MLGMma--VBwYiBZrdTDCKSdcOvJTgKPG/view"
-        },
-        {
-          title: "日度发电量快照表_10月25日",
-          description: "Daily power generation snapshot - October 25th",
-          url: "https://drive.google.com/file/d/1xhHoiXgljZxWc0-3yhdkLtcIPwMJ8nwP/view"
-        },
-        {
-          title: "日度发电量快照表_10月24日",
-          description: "Daily power generation snapshot - October 24th",
-          url: "https://drive.google.com/file/d/1hihkL6bZShU6FTTYRVhYizK5lmLMy9yO/view"
-        },
-        {
-          title: "日度发电量快照表_10月23日",
-          description: "Daily power generation snapshot - October 23rd",
-          url: "https://drive.google.com/file/d/156Eq2JfslzqFGXZARTXW67K6PFP8-7vJ/view"
-        },
-        {
-          title: "日度发电量快照表_10月22日",
-          description: "Daily power generation snapshot - October 22nd",
-          url: "https://drive.google.com/file/d/11Xud71w9T6z0rnn6s4Ai8Dbo31a9i1Oo/view?usp=drive_link"
-        },
-        {
-          title: "日度发电量快照表_10月21日",
-          description: "Daily power generation snapshot - October 21st",
-          url: "https://drive.google.com/file/d/1-HfyyP1W71rk-lvT0lKh0n7merZAx7u9/view"
-        },
-        {
-          title: "日度发电量快照表_10月20日",
-          description: "Daily power generation snapshot - October 20th",
-          url: "https://drive.google.com/file/d/1PMqwrCQm7ve3njjgSnw3X0ZcTR6jdXVb/view?usp"
-        },
-        {
-          title: "日度发电量快照表_10月19日",
-          description: "Daily power generation snapshot - October 19th",
-          url: "https://drive.google.com/file/d/125AHWlvlbE8OFrO-rc2Pc5Lo_p-4E9RL/view"
-        },
-        {
-          title: "日度发电量快照表_10月18日",
-          description: "Daily power generation snapshot - October 18th",
-          url: "https://drive.google.com/file/d/1p768iALX8NW7qQlpO_UpWUB1Apm6H8n8/view"
-        },
-        {
-          title: "日度发电量快照表_10月17日",
-          description: "Daily power generation snapshot - October 17th",
-          url: "https://drive.google.com/file/d/12DVuw0z8bPgrbmfl0yOYqXVM32Jaw-C6/view"
-        },
-        {
-          title: "日度发电量快照表_10月16日",
-          description: "Daily power generation snapshot - October 16th",
-          url: "https://drive.google.com/file/d/1TCmrvCB9OpHZzwA3Gn-uVaaa7HSBAuh0/view"
-        },
-        {
-          title: "日度发电量快照表_10月15日",
-          description: "Daily power generation snapshot - October 15th",
-          url: "https://drive.google.com/file/d/1NaFhzdMfNwRLCBxzsd3BECd-Jvv1t9FV/view"
-        },
-        {
-          title: "日度发电量快照表_10月14日",
-          description: "Daily power generation snapshot - October 14th",
-          url: "https://drive.google.com/file/d/1yTcYbA6Ljr-aLf8XtXDcrcbjkd3Q7apV/view"
-        },
-        {
-          title: "日度发电量快照表_10月13日",
-          description: "Daily power generation snapshot - October 13th",
-          url: "https://drive.google.com/file/d/14gZ_43lb-C7FpO0KsH-7rqRROntDPcdf/view"
-        },
-        {
-          title: "日度发电量快照表_10月12日",
-          description: "Daily power generation snapshot - October 12th",
-          url: "https://drive.google.com/file/d/1ospfRukexHunQNVKBVJveN5T8Qp_Ehlx/view"
-        },
-        {
-          title: "日度发电量快照表_10月11日",
-          description: "Daily power generation snapshot - October 11th",
-          url: "https://drive.google.com/file/d/1bK238GoE_vc9BktKnU7UF9MGZblLoB0O/view"
-        },
-        {
-          title: "日度发电量快照表_10月10日",
-          description: "Daily power generation snapshot - October 10th",
-          url: "https://drive.google.com/file/d/1WffubVm_LT8PObAYI7ddwbVkTrIr4Ixw/view"
-        },
-        {
-          title: "日度发电量快照表_10月09日",
-          description: "Daily power generation snapshot - October 9th",
-          url: "https://drive.google.com/file/d/1oMYEoBaEF_UWNOpV0LXqDZU9KcBaeHf4/view"
-        },
-        {
-          title: "日度发电量快照表_10月08日",
-          description: "Daily power generation snapshot - October 8th",
-          url: "https://drive.google.com/file/d/1ssYYjXZvYugxF-BpZNRycJGjwxKRU3CR/view"
-        },
-        {
-          title: "日度发电量快照表_10月07日",
-          description: "Daily power generation snapshot - October 7th",
-          url: "https://drive.google.com/file/d/1pKI5NsURP9dC2PmtswCtZi-gdlak64ky/view"
-        },
-        {
-          title: "日度发电量快照表_10月06日",
-          description: "Daily power generation snapshot - October 6th",
-          url: "https://drive.google.com/file/d/1OyuA6uvA9GwYo3M6ufqPFELgQibE_sn7/view"
-        },
-        {
-          title: "日度发电量快照表_10月05日",
-          description: "Daily power generation snapshot - October 5th",
-          url: "https://drive.google.com/file/d/1Y-PrXE2Ci-KJRbSGg8PzwoG19GV7kW_v/view"
-        },
-        {
-          title: "日度发电量快照表_10月04日",
-          description: "Daily power generation snapshot - October 4th",
-          url: "https://drive.google.com/file/d/1VjqqErRQmV1LVORx92iiXb_JLVfd0UmT/view"
-        },
-        {
-          title: "日度发电量快照表_10月03日",
-          description: "Daily power generation snapshot - October 3rd",
-          url: "https://drive.google.com/file/d/1GUlf-qhUjF4F9QtJguUh8KV7h2VFTrhH/view"
-        },
-        {
-          title: "日度发电量快照表_10月02日",
-          description: "Daily power generation snapshot - October 2nd",
-          url: "https://drive.google.com/file/d/14hrBdDW2J-YVkhokPRWLk--FqvArKhQa/view"
-        }
-      ]
+      // show first N in the grid; the rest are inside a collapsible <details> rendered in the description area
+      description: (
+        <>
+          <span>Daily power generation snapshots and verification documentation (first {visibleLimit} shown; expand to view more)</span>
+          {onChainMore && (
+            <details className="mt-4">
+              <summary className="text-sm text-primary cursor-pointer">Show {onChainMore.length} more snapshots</summary>
+              <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {onChainMore.map((doc) => (
+                  <DocumentCard
+                    key={doc.title}
+                    title={doc.title}
+                    description={doc.description}
+                    url={doc.url}
+                    type="PDF"
+                  />
+                ))}
+              </div>
+            </details>
+          )}
+        </>
+      ),
+      documents: onChainVisible
     },
     {
       title: "Integrated Financial Model & Tokenized Valuation",
       description: "Comprehensive financial models and tokenized valuation documentation",
       documents: [
-        {
-          title: "Financial-Audit-and-Valuation-Index",
-          description: "Comprehensive financial audit and valuation index documentation",
-          url: "https://drive.google.com/file/d/1zSuRXGfmgzzgb_yt6pM_rWHab6JX1fmB/view"
-        },
         {
           title: "Dual_Token_rGGP_+_GVT_(Whitepaper_Excerpts)_v2025.10",
           description: "Dual token system whitepaper excerpts covering rGGP and GVT tokens",
